ae042082175361295b23aee2594190b65bcc72c9
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-04-04 20:12:22 +0600

    reating added

--- src/Components/ReviewDetails/ReviewDetails.js
+++ src/Components/ReviewDetails/ReviewDetails.js
@@ -1,12 +1,20 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Rating from 'react-rating';
 
 const ReviewDetails = ({ data }) => {
     const { name, reviews, reviewsDetails } = data;
     return (
         <div className='border p-7 shadow-2xl rounded bg-gray-100'>
-            <h1> <small className='font-bold'>Name:</small> {name}</h1>
-            <p><small className='font-bold'>Reviews:</small> {reviews}</p>
-            <p><small className='font-bold text-[16px]'>ReviewsDetails:</small> {reviewsDetails}</p>
+            <h1> <small className='font-bold'>Name:</small> <small className='text-xl'>{name}</small></h1>
+            <p><small>Reviews:</small><Rating
+                initialRating={reviews}
+                emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                fullSymbol={<FontAwesomeIcon style={{ color: 'goldenrod' }} icon={faStar} />}
+                readonly
+            ></Rating></p>
+            <p><small>ReviewsDetails : </small> {reviewsDetails}</p>
         </div>
     );
 };

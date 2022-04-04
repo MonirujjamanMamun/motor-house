f3e987b5ef22bf5a92da9b7a0a1412728aba6bf1
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-04-04 18:14:52 +0600

    two question answered

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is blogs section.</h1>
+        <div className='grid grid-cols-3 gap-3'>
+            <div className='border-2 shadow-2xl m-5 p-7 rounded'>
+                <h1 className='text-2xl font-bold my-2'>What is Context API?</h1>
+                <p>The react context api is a way for a react app to effectively produce global variables that can be passed around. This is  the alternative to prop drilling or moving props from grandparent to child to parent and so on. Context is also touted as an  easier ,lighter approach to state management using redux.</p>
+            </div>
+            <div className='border-2 shadow-2xl m-5 p-7 rounded'>
+                <h1 className='text-2xl font-bold my-2'>What is Semantic Tag?</h1>
+                <p> A semantic element clearly describes its meaning to both the browser and the developer. Semantic tag are two kings.
+                    <li> (i) <span className='font-bold'>Non-semantics tag : </span> for example div, span, small, h1-h6, p etc.</li>
+                    <li>(ii) <span className='font-bold'>Semantics Tag : </span> for example form, tabil, article, aside, footer, nav, section etc.</li>
+                </p>
+            </div>
         </div>
     );
 };
